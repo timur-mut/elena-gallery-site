2394cffb88e2106b932d48e92eccf9074f78bae3
timur-mut <timur-mut@live.com>
2020-05-23 15:24:35 +0500

    Updates

---
+++ component---src-pages-404-js-db7772fe0974e7f54f71.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),c=t.n(a),o=t("vOnD"),i=t("L12J"),p=o.c.h1.withConfig({displayName:"sc-404__Message",componentId:"sc-6eb995-0"})(["height:calc(50vh + 18px);font-weight:normal;margin:0;padding-top:calc(50vh - 18px);font-size:36px;text-align:center;letter-spacing:4px;"]);e.default=function(){return c.a.createElement(i.a,{title:"404 not found"},c.a.createElement(p,null,"404 not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-db7772fe0974e7f54f71.js.map
